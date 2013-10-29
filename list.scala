2d739b3dcf70395861d228db4ae0be4c0e95dddb
hiroki.oiwa <hiroki.oiwa@gree.net>
2013-10-29 11:00:02 +0900

    array

---
+++ list.scala
@@ -0,0 +1,14 @@
+object list {
+
+  def main(args: Array[String]) {
+    val a = 10
+    val b = 20
+    val c = 30
+
+    println(a + b + c)
+
+    val arr = Array[Int](10, 20, 30)
+    println(arr(0) + "," + arr(1) + "," + arr(2))
+  }
+
+}
