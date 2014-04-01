50b882a378969f926e35c1642d7a9c77b810e861
funnythingz <hiroki@hackleberry.me>
2014-04-01 23:38:06 +0900

    updated

--- list.scala
+++ list.scala
@@ -1,14 +1,19 @@
 object list {
 
-  def main(args: Array[String]) {
-    val a = 10
-    val b = 20
-    val c = 30
+    def main(args: Array[String]) {
 
-    println(a + b + c)
+        val a: Int = 10
+        val b: Int = 20
+        val c: Int = 30
 
-    val arr = Array[Int](10, 20, 30)
-    println(arr(0) + "," + arr(1) + "," + arr(2))
-  }
+        println(sum(a, b, c))
+
+        val arr = Array[Int](a, b, c)
+        println(arr.length)
+        println(arr(0) + "," + arr(1) + "," + arr(2))
+
+    }
+
+    def sum(a: Int, b: Int, c: Int): Int = a + b + c
 
 }
